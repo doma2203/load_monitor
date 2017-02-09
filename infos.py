6a7bff0cdc74519ad94a71fafb80444cf02e57b3
Dominika Kałafut <dkkalafut@gmail.com>
2017-02-09 21:54:36 +0100

    Ending parser

--- infos.py
+++ infos.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
 import psutil
-from functools import wraps
-from glob import glob, iglob
+from glob import glob
 import os
 import os.path
 import re
 import time
+import pySMART
 
+
+'''Wersja obiektowa kodu jest skonstruowana tak, aby jak najmniej czesci wspolnych dla modulu sie powtarzalo,
+zeby mozliwa byla specjalizacja jednego konkretnego elementu (klasy) w kontekscie wielu odczytow, poprzez dziedziczenie
+i funkcje wirtualne. Kazdy odczyt posiada te same metody, ale dla kazdego elementu realizowane sa inaczej, z uwagi na
+rozne pliki, z ktorych czytaja oraz inny sposob etykietowania i nazywania konkretnych odczytow. Kod z tego powodu moze
+wydawac sie troche dluzszy, ale jest sporo czytelniejszy (szczegolnie widac to na przykladzie temperatury). Wszystkie
+klasy poszczegolnych parametrow sa "callable" - zeby mozliwe bylo zwrocenie konkretnych wartosci, a te, ktore
+zorganizowane sa w bardziej zlozone typy danych - takze "iterable", zeby uzytkownik mogl sobie intuicyjnie iterowac
+po rdzeniach, procesach i obszarach monitorowania temperatury. I, oczywiscie, posiadaja ulubione i specjalnie dedykowane
+'property'.'''
 
 class MonitorPoint(object):
     def __init__(self):
@@ -31,7 +41,7 @@ class MonitorPoint(object):
         self._value=val
 
     def __call__(self, *args, **kwargs):
-        '''Zakladam, ze w najprostszym przypadku etykietki brak '''
+        '''Zakladam, ze w najprostszym przypadku etykietki brak. Tak jest na przyklad z Uptime()'''
         return self.value
 
     def check(self):
@@ -58,13 +68,14 @@ class Temperature(MonitorPoint):
         try:
             return float(file.readline())/1000
         except IOError:
+        # problemy z niektorymi pery
             return 0
 
     def checklabel(self):
         '''Jako glowny parametr klasy dostajemy plik z odczytem poniewaz zakladamy, ze takowy bedzie istnial zawsze,
         czego nie mozna powiedziec np. o etykietkach. Jedyna zmiana, jaka musimy poczynic, zeby plik z odczytem zmienil
-        sie w plik z etykieta (ktorego, obecnosc w systemie plikow weryfikujemy), jest podmiana czesci
-        sciezki za pomoca re.sub-a.'''
+        sie w plik kandydujacy do pliku z etykieta (nie jestesmy 100% pewni, gdzie i co posiada etykiety, stad exist())
+        jest podmiana czesci sciezki za pomoca re.sub-a.'''
         lab=re.sub(r'(input)','label',self.path)
         if os.path.exists(lab):
             file=open(lab)
@@ -140,13 +151,13 @@ class Frequency(MonitorPoint):
         return re.findall(r'(cpu\d)',self.path)[0]
 
 
-class FrequencyPoints:
-    def __init__(self,dirpath):
-        self._dirpath=dirpath
-        self._cores=[Frequency(path)() for path in glob(self.dirpath)]
+class Cores:
+    def __init__(self):
+        self._dirpath='/sys/bus/cpu/devices/cpu?/cpufreq/'
+        self._cores=self.sortcores()
 
-     def __call__(self, *args, **kwargs):
-         return self.cores
+    def __call__(self, *args, **kwargs):
+        return self.cores
 
     def __iter__(self):
         for core in self.cores:
@@ -155,6 +166,11 @@ class FrequencyPoints:
     @property
     def cores(self):
         return self._cores
+
+    def sortcores(self):
+        # Wypadalo posortowac...
+        cores=[Frequency(path)() for path in glob(self._dirpath)]
+        return sorted(cores,key=lambda m: m[0])
 
 
 class Processes:
@@ -178,9 +194,126 @@ class Processes:
         return self._pslist
 
     def processlist(self):
+        '''Po wyjasnienia 'dlaczego tak nieczytelnie' zapraszam do dokumentacji psutil, czytelniej sie nie dalo.
+        Lambda to handmade, sortowanie jest przeprowadzane poprawnie, nawet jesli nie uruchamiamy jej w petli
+        (od niej zalezy cpu_percent), bo sortuje wtedy wedlug zuzucia RAMunit-u, a potem przez przydzial pamieci :)
+        Zwracamy liste (wazna kolejnosc!!!) skladajaca sie z nazwy i PID-u - umozliwia to szybkie reczne 'ubicie'
+        procesu naduzywajacego naszej goscinnosci... '''
         proclist=list()
         for ps in psutil.process_iter():
             proclist.append(ps.as_dict(attrs=['pid', 'name', 'memory_percent', 'memory_info', 'cpu_percent']))
-        proclist=sorted(proclist, key=lambda k: (k['cpu_percent'], k['memory_percent'],
-                                                     k['memory_info'][0], k['memory_info'][1]), reverse=True)[:self.number]
+        proclist=sorted(proclist, key=lambda k: (k['cpu_percent'], k['memory_percent'], k['memory_info'][0],
+                                                 k['memory_info'][1]), reverse=True)[:self.number]
         return [(proc['name'],proc['pid']) for proc in proclist]
+
+
+class RAMunit(MonitorPoint):
+    def __init__(self,mode):
+        self._mode=mode
+        super(RAMunit, self).__init__()
+
+    def __call__(self, *args, **kwargs):
+        return self.mode,self.value
+
+
+    @property
+    def mode(self):
+        return self._mode
+
+    def check(self):
+        ''' Co sie tu dzieje: Wyrazenia regularne typu: "positive lookbehind assertion" nie przyjmuja w czesci
+        okreslajacej poprzednik zmiennej dlugosci wyrazow (w tym wypadku spacji),wiec nie przejdzie ani
+         \s{4,11}, ani tym bardziej \s+. Ilosc spacji zalezy od... dlugosci liczby okreslajacej pamiec w kB, wiec
+         trzeba cyklicznie rzucac wyjatkiem i go obslugiwac, zmieniajac wyrazenie. '''
+        file=open('/proc/meminfo')
+        info=file.readlines()
+        if self.mode is 'total':
+            return self.findsize(10,5,'MemTotal',info[0])
+        elif self.mode is 'free':
+            # nie wiem, kto wymyslil taka ilosc spacji, wklepalam z autopsji :D
+            return self.findsize(11,4,'MemFree',info[1])
+        else:
+            raise NotImplementedError
+
+    def findsize(self,upperbound, lowerbound, parameter,text):
+        for spacenum in range(upperbound,lowerbound,-1):
+            regexp=r'(?<='+parameter+':\s{'+str(spacenum)+'})(\d+)'
+            try:
+                res=re.search(regexp,text)
+                return float(res.group(0))/1024
+            except AttributeError:
+                continue
+
+
+class RAM:
+    def __init__(self):
+        self._avaliable=RAMunit('total')
+        self._free=RAMunit('free')
+
+    def __call__(self, *args, **kwargs):
+        return self.available.__call__(),self.free.__call__()
+
+    @property
+    def available(self):
+        return self._avaliable
+
+    @property
+    def free(self):
+        return self._free
+
+
+class Battery(MonitorPoint):
+    def __init__(self,path):
+        self._path=path
+        super(Battery,self).__init__()
+
+    def __call__(self, *args, **kwargs):
+        return self.path,self.value
+
+    @property
+    def path(self):
+        return self._path
+
+    def check(self):
+        if os.path.exists(self.path+'/capacity'):
+            file=open(self.path+'/capacity')
+            return float(file.readline())
+
+    def checklabel(self):
+        if os.path.exists(self.path):
+            battlab=re.search('(?<=power_supply/)(\w{3}\d)', self.path)
+            return battlab.group(0)
+
+
+class Batteries:
+    def __init__(self):
+        self._path='/sys/class/power_supply/BAT?'
+        self._batlist=[Battery(i)() for i in glob(self.path)]
+
+    def __call__(self, *args, **kwargs):
+        return self.batterylist
+
+    def __iter__(self):
+        for battery in self.batterylist:
+            yield battery
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def batterylist(self):
+        return self._batlist
+
+
+class HDDTemp:
+    pass
+
+
+for i in Cores():
+    print i
+
+ram=RAM()
+print ram()
+print Batteries()()
+
